83bdcd0da45903211273beb3d618922c8297386b
Arijeet Konar <43075962+disguised-devil@users.noreply.github.com>
2018-11-12 22:46:17 +0530

    Add files via upload

---
+++ CDES2.C
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char ch[50], ch1[50];
+int i=0,k=0;
+int l;
+clrscr();
+printf("Enter the production \n");
+gets(ch);
+for(i=0;ch[i]!='\0';i++)
+{
+	if(ch[i]>='A' && ch[i]<='Z')
+	{
+		ch1[k]=ch[i];
+		k++;
+	}
+}
+	printf("The non terminals are \n");
+	for(l=65;l<=91;l++)
+	{
+		for(k=0;ch1[k]!='\0';k++)
+		{
+			if((char)l==ch1[k])
+			{
+				printf("%c",ch1[k]);
+				printf("\n");
+				break;
+			}
+		}
+	}
+getch();
+}
